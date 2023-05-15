13236af64bef2e8fdce56c6b2baad6267f7848ef
Akiho Kawada <kawadatakeshiho@gmail.com>
2023-05-16 04:42:02 +0900

    feat: スクリプト追加

---
+++ first_week/homework1/anagram.py
@@ -0,0 +1,40 @@
+import sys
+
+
+def judge_anagram(target, sorted_words_list):
+    low = 0
+    high = len(sorted_words_list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if sorted_words_list[mid] == target:
+            return mid
+        elif sorted_words_list[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
+def find_value_by_key(key, data_list):
+    for data in data_list:
+        if data[0] == key:
+            return data[1]
+    return False
+
+
+def main(given_word):
+    target = "".join(sorted(given_word))
+
+    with open("/Users/takeshiho0531//Desktop/STEP2023/first_week/homework1/sorted_words.txt", "r") as f:
+        sorted_words_set_list = f.read().splitlines()
+
+    sorted_words_list = []
+    for sorted_words_set in sorted_words_set_list:
+        sorted_words_list.append(sorted_words_set[0])
+
+    anagram_judgment = judge_anagram(target, sorted_words_list)
+    if anagram_judgment == -1:
+        return False
+    else:
+        answer = find_value_by_key(target, sorted_words_set_list)
+        return answer
---
+++ first_week/homework1/save_dict.py
@@ -0,0 +1,27 @@
+import json
+
+
+def sort_words_mapping(words_list: list) -> list[tuple[str, str]]:
+    sorted_word_set_list = []
+    for word in words_list:
+        sorted_word = "".join(sorted(word))
+        sorted_word_set_list.append((sorted_word, word))
+    return sorted_word_set_list
+
+
+def main():
+    with open("~/Desktop/STEP2023/first_week/homework1/words.txt", "r") as f:
+        words_list = f.read().splitlines()
+
+    sorted_words_set_list = sort_words_mapping(words_list)
+
+    with open(
+        "~/Desktop/STEP2023/first_week/homework1/sorted_words.txt",
+        "w",
+    ) as file:
+        for mapped_words in sorted_words_set_list:
+            file.write(str(mapped_words) + "\n")
+
+
+if __name__ == "__main__":
+    main()
