54d1809fba2b5a3f749042244b4e0e1f47272653
Akiho Kawada <kawadatakeshiho@gmail.com>
2023-05-17 01:23:58 +0900

    wip: とりあえずscore返すところまで

--- first_week/homework2/score_checker.py
+++ first_week/homework2/score_checker.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import sys
+from typing import Optional
 
 # How to use:
 #
@@ -16,23 +17,19 @@ import sys
 # ----------------------------------------
 SCORES = [1, 3, 2, 2, 1, 3, 3, 1, 1, 4, 4, 2, 2, 1, 1, 3, 4, 1, 1, 1, 2, 3, 3, 4, 3, 4]
 
-WORDS_FILE = "words.txt"
+WORDS_FILE_PATH = "first_week/homework2/words.txt"
 
-# def calculate_score(word):
-    # score = 0
-    # for character in list(word):
-        # score += SCORES[ord(character) - ord('a')]
-    # return score
 
-def read_words(word_file):
+def read_words(word_file_path):
     words_list = []
-    with open(word_file) as f:
+    with open(word_file_path) as f:
         for line in f:
-            line = line.rstrip('\n')
+            line = line.rstrip("\n")
             words_list.append(line)
     return words_list
 
-def calculate_score(valid_word: str, data: str) -> int:
+
+def calculate_score(valid_word: str, data: str) -> Optional[int]:
     """dataを並び替えた時に意味のある単語を含むか判定するして、その時のスコアを返す関数
 
     Args:
@@ -45,63 +42,37 @@ def calculate_score(valid_word: str, data: str) -> int:
     score = 0
     data_table = [0] * 26
     for character in data:
-        data_table[ord(character) - ord('a')] += 1
+        data_table[ord(character) - ord("a")] += 1
     # ex. data="happy" -> data_table=[1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
     for character in valid_word:
-        if (data_table[ord(character) - ord('a')] == 0): # そもそもvalid_wordにその文字が含まれてなかったらだめ
+        if (
+            data_table[ord(character) - ord("a")] == 0
+        ):  # そもそもvalid_wordにその文字が含まれてなかったらだめ
             score = 0
             break
-        data_table[ord(character) - ord('a')] -= 1
-        score += SCORES[ord(character) - ord('a')]
+        data_table[ord(character) - ord("a")] -= 1
+        score += SCORES[ord(character) - ord("a")]
     return score
 
 
-def get_highest_score_and_word(valid_words_list, data_list):
-    highest_score = 0
-    highest_score_word = ""
-    for valid_word in valid_words_list:
-        score_by_word = 0
-        for data in data_list:
-            score = calculate_score(valid_word, data)
-            if score > highest_score:
-                highest_score = score
-                highest_score_word = valid_word
-    return highest_score_word, highest_score
-
-def get_highest_scores_and_word(valid_words_list, data_list):
-    highest_score = 0
-    highest_score_word = ""
-    for valid_word in valid_words_list:
-        score_by_data_word = 0
-        scores_by_word_list=[]
-        for data in data_list:
-            score = calculate_score(valid_word, data)
-            score_by_data_word += score
-            scores_by_word_list.append(score_by_data_word)
-
+def get_highest_scores(valid_words_list, data_list):
+    sum_score = 0
     for data in data_list:
-        score_by_data_word = 0
-        highest_score_by_word=0
+        highest_score = 0
         for valid_word in valid_words_list:
             score = calculate_score(valid_word, data)
-            score_by_data_word += score
-        if score_by_data_word > highest_score_by_word:
-            highest_score_by_word = score_by_data_word
-            highest_score_word = data
-
-    return highest_score_word, highest_score_by_word
+            if highest_score < score:
+                highest_score = score
+        sum_score += highest_score
+    return sum_score
 
 
 def main(data_file_path):
-    valid_words_list = read_words(WORDS_FILE)
+    valid_words_list = read_words(WORDS_FILE_PATH)
     data_words_list = read_words(data_file_path)
-    highest_score_word, highest_score = get_highest_scores_and_word(valid_words_list, data_words_list)
-    print('You answer is correct! Your score is %d.' % highest_score)
-    print(highest_score_word)
+    highest_score = get_highest_scores(valid_words_list, data_words_list)
+    print("You answer is correct! Your score is %d." % highest_score)
+
 
 if __name__ == "__main__":
-    # if len(sys.argv) != 3:
-        # print("usage: %s data_file your_answer_file" % sys.argv[0])
-        # exit(1)
     main(sys.argv[1])
-    #main(sys.argv[1], sys.argv[2])
