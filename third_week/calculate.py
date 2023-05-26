dbb7796cb570c1942cb905da110b225d5a14ead8
Akiho Kawada <kawadatakeshiho@gmail.com>
2023-05-27 01:50:07 +0900

    feat: とりあえず一旦かっこありの機能もつけた

--- third_week/calculate.py
+++ third_week/calculate.py
@@ -1,4 +1,12 @@
-from read_letter import read_number, read_plus, read_minus, read_multiply, read_divide, get_string_in_parentheses
+from third_week.read_letter import (
+    parse_by_parentheses,
+    read_divide,
+    read_minus,
+    read_multiply,
+    read_number,
+    read_plus,
+)
+
 
 def tokenize(letters):
     tokens = []
@@ -6,46 +14,64 @@ def tokenize(letters):
     while index < len(letters):
         if letters[index].isdigit():
             (token, index) = read_number(letters, index)
-        elif letters[index] == '+':
+        elif letters[index] == "+":
             (token, index) = read_plus(letters, index)
-        elif letters[index] == '-':
+        elif letters[index] == "-":
             (token, index) = read_minus(letters, index)
-        elif letters[index] == '*':
+        elif letters[index] == "*":
             (token, index) = read_multiply(letters, index)
-        elif letters[index] == '/':
+        elif letters[index] == "/":
             (token, index) = read_divide(letters, index)
         else:
-            print('Invalid character found: ' + letters[index])
+            print("Invalid character found: " + letters[index])
             exit(1)
         tokens.append(token)
     return tokens
 
 
-def calculate(tokens):
+def calculate_by_token(tokens):
     answer = 0
-    tokens.insert(0, {'type': "PLUS"}) # Insert a dummy "+" token
-    index=1
-    while index<len(tokens):
-        if tokens[index]["type"]=="NUMBER":
-            if tokens[index-1]["type"]=="Multiply":
-                answer*=tokens[index]["number"]
-            elif tokens[index-1]["type"]=="DEVIDE":
-                answer/=tokens[index]["number"]
-        index+=1
-    while index<len(tokens):
-        if tokens[index]["type"]=="NUMBER":
-            if tokens[index-1]["type"]=="PLUS":
-                answer+=tokens[index]["number"]
-            elif tokens[index-1]["type"]=="MINUS":
-                answer-=tokens[index]["number"]
-        index+=1
+    tokens.insert(0, {"type": "PLUS"})  # Insert a dummy "+" token
+    index = 1
+    while index < len(tokens):
+        if tokens[index]["type"] == "NUMBER":
+            if tokens[index - 1]["type"] == "Multiply":
+                answer *= tokens[index]["number"]
+            elif tokens[index - 1]["type"] == "DEVIDE":
+                answer /= tokens[index]["number"]
+        index += 1
+    while index < len(tokens):
+        if tokens[index]["type"] == "NUMBER":
+            if tokens[index - 1]["type"] == "PLUS":
+                answer += tokens[index]["number"]
+            elif tokens[index - 1]["type"] == "MINUS":
+                answer -= tokens[index]["number"]
+        index += 1
     return answer
 
+
+def calculate(letters):
+    tokens_list = tokenize(letters)
+    answer = calculate_by_token(tokens_list)
+    return answer
+
+
+def integrate(whole_letters, parentheses_index_list):
+    answer = 0
+    for i in range(len(parentheses_index_list)):
+        parentheses_index = parentheses_index_list[i]
+        answer_by_parse = calculate(
+            whole_letters[(parentheses_index[0] + 1) : (parentheses_index[1] - 1)]
+        )
+        whole_letters[parentheses_index[0] + 2] = "."
+        whole_letters[(parentheses_index[0] + 3) : (parentheses_index[1] - 1)] = "0"
+        answer += answer_by_parse
+    return answer
 
 
 def main():
     while True:
-        letters=input()
-        tokens_list=tokenize(letters)
-        answer=calculate(tokens_list)
+        whole_letters = input()
+        parentheses_index_list = parse_by_parentheses(whole_letters)
+        answer = integrate(whole_letters, parentheses_index_list)
     print("answer = %d\n" % answer)
--- third_week/read_letter.py
+++ third_week/read_letter.py
@@ -1,35 +1,49 @@
 def read_number(letters, index):
     number = 0
-    if letters[index]==".":
-        decimal_point_index=index
+    if letters[index] == ".":
+        decimal_point_index = index
 
-    while index<len(letters) and (letters[index].isdigit() or letters[index]=="."):
-        if letters[index]==".":
+    while index < len(letters) and (letters[index].isdigit() or letters[index] == "."):
+        if letters[index] == ".":
             continue
-        number=number*10+int(letters[index])
-        index+=1
+        number = number * 10 + int(letters[index])
+        index += 1
 
-    number=number/10**(len(letters)-decimal_point_index-1)
-    token = {'type': 'Number', 'number': number}
+    number = number / 10 ** (len(letters) - decimal_point_index - 1)
+    token = {"type": "Number", "number": number}
     return token, index
 
 
 def read_plus(index):
-    token = {'type': 'PLUS'}
-    return token, index+1
+    token = {"type": "PLUS"}
+    return token, index + 1
+
 
 def read_minus(index):
-    token = {'type': 'MINUS'}
-    return token, index+1
+    token = {"type": "MINUS"}
+    return token, index + 1
+
 
 def read_multiply(index):
-    token = {'type': 'MULTIPLY'}
-    return token, index+1
+    token = {"type": "MULTIPLY"}
+    return token, index + 1
+
 
 def read_divide(index):
-    token = {'type': 'DEVIDE'}
-    return token, index+1
+    token = {"type": "DEVIDE"}
+    return token, index + 1
 
-def get_string_in_parentheses():
-    pass
 
+def parse_by_parentheses(letters):
+    letter_list = []
+    starting_parenthese_index_list = []
+    parentheses_index_list = []
+    for i in range(len(letters)):
+        letter_list.append(letters[i])
+        if letters[i] == "(":
+            starting_parenthese_index_list.append(i)
+        if letters[i] == ")":
+            starting_parenthese_index = starting_parenthese_index_list[-1]
+            parentheses_index_list.append((starting_parenthese_index, i))
+            starting_parenthese_index_list = starting_parenthese_index_list[:-1]
+    return parentheses_index_list
