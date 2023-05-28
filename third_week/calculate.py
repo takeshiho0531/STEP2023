c7d1bb41c64904533dede3216e4eb5d0dd957f47
takeshiho0531 <kawadatakeshiho@gmail.com>
2023-05-29 04:18:49 +0900

    fix: とりあえずbugは取ったけど、小数点の分を後から割るってのはやめたほうが良い、indexでのミスが起きやすすぎる

--- third_week/calculate.py
+++ third_week/calculate.py
@@ -1,5 +1,13 @@
-from third_week.read_letter import (
-    parse_by_parentheses,
+# from third_week.read_letter import (
+    # parse_by_parentheses,
+    # read_divide,
+    # read_minus,
+    # read_multiply,
+    # read_number,
+    # read_plus,
+# )
+from read_letter import (
+        parse_by_parentheses,
     read_divide,
     read_minus,
     read_multiply,
@@ -8,12 +16,14 @@ from third_week.read_letter import (
 )
 
 
+
 def tokenize(letters):
     tokens = []
     index = 0
     while index < len(letters):
-        if letters[index].isdigit():
+        if letters[index].isdigit() or letters[index]==".":
             (token, index) = read_number(letters, index)
+        #elif letters[index] == "+":
         elif letters[index] == "+":
             (token, index) = read_plus(letters, index)
         elif letters[index] == "-":
@@ -25,6 +35,7 @@ def tokenize(letters):
         else:
             print("Invalid character found: " + letters[index])
             exit(1)
+        print("token", token)
         tokens.append(token)
     return tokens
 
@@ -34,16 +45,19 @@ def calculate_by_token(tokens):
     tokens.insert(0, {"type": "PLUS"})  # Insert a dummy "+" token
     index = 1
     while index < len(tokens):
-        if tokens[index]["type"] == "NUMBER":
+        if tokens[index]["type"] == "Number":
             if tokens[index - 1]["type"] == "Multiply":
                 answer *= tokens[index]["number"]
             elif tokens[index - 1]["type"] == "DEVIDE":
                 answer /= tokens[index]["number"]
         index += 1
+        print("answer1", answer)
+    index=1
     while index < len(tokens):
-        if tokens[index]["type"] == "NUMBER":
+        if tokens[index]["type"] == "Number":
             if tokens[index - 1]["type"] == "PLUS":
                 answer += tokens[index]["number"]
+                print("index",index)
             elif tokens[index - 1]["type"] == "MINUS":
                 answer -= tokens[index]["number"]
         index += 1
@@ -51,21 +65,27 @@ def calculate_by_token(tokens):
 
 
 def calculate(letters):
+    print("letters",letters)
     tokens_list = tokenize(letters)
+    print(tokens_list)
     answer = calculate_by_token(tokens_list)
+    print("answer", answer)
     return answer
 
 
 def integrate(whole_letters, parentheses_index_list):
     answer = 0
-    for i in range(len(parentheses_index_list)):
-        parentheses_index = parentheses_index_list[i]
-        answer_by_parse = calculate(
-            whole_letters[(parentheses_index[0] + 1) : (parentheses_index[1] - 1)]
-        )
-        whole_letters[parentheses_index[0] + 2] = "."
-        whole_letters[(parentheses_index[0] + 3) : (parentheses_index[1] - 1)] = "0"
-        answer += answer_by_parse
+    if parentheses_index_list==[]:
+        answer = calculate(whole_letters)
+    else:
+        for i in range(len(parentheses_index_list)):
+            parentheses_index = parentheses_index_list[i]
+            answer_by_parse = calculate(
+                whole_letters[(parentheses_index[0] + 1) : (parentheses_index[1] - 1)]
+            )
+            whole_letters[parentheses_index[0] + 2] = "."
+            whole_letters[(parentheses_index[0] + 3) : (parentheses_index[1] - 1)] = "0"
+            answer += answer_by_parse
     return answer
 
 
@@ -75,3 +95,22 @@ def main():
         parentheses_index_list = parse_by_parentheses(whole_letters)
         answer = integrate(whole_letters, parentheses_index_list)
     print("answer = %d\n" % answer)
+
+def test(line):
+    #tokens = tokenize(line)
+    print("line", line)
+    parentheses_index_list = parse_by_parentheses(line)
+    actual_answer = integrate(line, parentheses_index_list)
+    expected_answer = eval(line)
+    if abs(actual_answer - expected_answer) < 1e-8:
+        print("PASS! (%s = %f)" % (line, expected_answer))
+    else:
+        print("FAIL! (%s should be %f but was %f)" % (line, expected_answer, actual_answer))
+
+def run_test():
+    print("==== Test started! ====")
+    test("1+2")
+    test("1.0+2.1-3")
+    print("==== Test finished! ====\n")
+
+run_test()
--- third_week/read_letter.py
+++ third_week/read_letter.py
@@ -1,35 +1,46 @@
+# def read_number(letters, index):
 def read_number(letters, index):
     number = 0
-    if letters[index] == ".":
-        decimal_point_index = index
+    # if letters[index] == ".":
+        # decimal_point_index = index
+    #decimal_point_index = len(letters)
+    #print("decimal_point_index_0", decimal_point_index)
 
+    decimal_point_index = None
+    # print("decimal_point_index_0", decimal_point_index)
+    # index=0
     while index < len(letters) and (letters[index].isdigit() or letters[index] == "."):
         if letters[index] == ".":
-            continue
-        number = number * 10 + int(letters[index])
+            # continue
+            decimal_point_index = index
+        else:
+            number = number * 10 + int(letters[index])
         index += 1
-
-    number = number / 10 ** (len(letters) - decimal_point_index - 1)
+    if decimal_point_index==None:
+        decimal_point_index = index-1
+    print("decimal_point_index", decimal_point_index)
+    print("nubmer_1", number)
+    number = number / 10 ** (index - decimal_point_index-1)
     token = {"type": "Number", "number": number}
     return token, index
 
 
-def read_plus(index):
+def read_plus(letters, index):
     token = {"type": "PLUS"}
     return token, index + 1
 
 
-def read_minus(index):
+def read_minus(letters, index):
     token = {"type": "MINUS"}
     return token, index + 1
 
 
-def read_multiply(index):
+def read_multiply(letters, index):
     token = {"type": "MULTIPLY"}
     return token, index + 1
 
 
-def read_divide(index):
+def read_divide(letters, index):
     token = {"type": "DEVIDE"}
     return token, index + 1
 
@@ -46,4 +57,5 @@ def parse_by_parentheses(letters):
             starting_parenthese_index = starting_parenthese_index_list[-1]
             parentheses_index_list.append((starting_parenthese_index, i))
             starting_parenthese_index_list = starting_parenthese_index_list[:-1]
+    print("parentheses_index_list", parentheses_index_list)
     return parentheses_index_list
