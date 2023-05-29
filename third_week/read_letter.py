87328e9921e8b4cc46253868074352818576e3b9
takeshiho0531 <kawadatakeshiho@gmail.com>
2023-05-30 01:48:42 +0900

    feat: 一旦数字読む部分書き直した

--- third_week/read_letter.py
+++ third_week/read_letter.py
@@ -1,61 +1,74 @@
-# def read_number(letters, index):
-def read_number(letters, index):
-    number = 0
-    # if letters[index] == ".":
-        # decimal_point_index = index
-    #decimal_point_index = len(letters)
-    #print("decimal_point_index_0", decimal_point_index)
+def get_digits(line: str, start_idx: int) ->dict:
+    """_summary_
 
-    decimal_point_index = None
-    # print("decimal_point_index_0", decimal_point_index)
-    # index=0
-    while index < len(letters) and (letters[index].isdigit() or letters[index] == "."):
-        if letters[index] == ".":
-            # continue
-            decimal_point_index = index
-        else:
-            number = number * 10 + int(letters[index])
-        index += 1
-    if decimal_point_index==None:
-        decimal_point_index = index-1
-    print("decimal_point_index", decimal_point_index)
-    print("nubmer_1", number)
-    number = number / 10 ** (index - decimal_point_index-1)
-    token = {"type": "Number", "number": number}
-    return token, index
+    Args:
+        line (str): _description_
+        start_idx (int): NUMERICAL(数字部分)が始まるindex
 
+    Returns:
+        dict: _description_
+    """
 
-def read_plus(letters, index):
-    token = {"type": "PLUS"}
-    return token, index + 1
+    digits = ['0','1','2', '3', '4', '5', '6', '7', '8', '9', '.']
+    idx=start_idx
+    digit_list=[]
+    while idx in digits:
+        digit_list.append(line[idx])
+        idx+=1
+    next_idx =idx
+    return next_idx, digit_list
 
-
-def read_minus(letters, index):
-    token = {"type": "MINUS"}
-    return token, index + 1
+def digits_to_number(digit_list):
+    idx=0
+    number =0
+    while digit_list[idx]!=".":
+        number=number*10+digit_list[idx]
+        idx+=1
+    if digit_list[idx]==".":
+        decimal_point_idx=idx
+        idx+=1
+        while idx<len(digit_list):
+            number+=digit_list[idx]*(10**(idx-decimal_point_idx))
+            idx+=1
+    return number
 
 
-def read_multiply(letters, index):
-    token = {"type": "MULTIPLY"}
-    return token, index + 1
+def read_digits(line: str, start_idx: int):
+    # assert line[start_idx]
+    next_idx, digit_list = get_digits(line, start_idx)
+    number = digits_to_number[digit_list]
+    return {"type": "NUMBER", "value": number, "next_idx": next_idx}
+
+def read_plus(line: str, start_idx: int):
+    assert line[start_idx]=="+"
+    return {"type": "PLUS", "next_idx": start_idx+1}
+
+def read_minus(line: str, start_idx: int):
+    assert line[start_idx]=="-"
+    return {"type": "MINUS", "next_idx": start_idx+1}
+
+def read_multiply(line: str, start_idx: int):
+    assert line[start_idx]=="*"
+    return {"type": "MULTIPLY", "next_idx": start_idx+1}
+
+def read_divide(line: str, start_idx: int):
+    assert line[start_idx]=="/"
+    return {"type": "DIVIDE", "next_idx": start_idx+1}
+
+def get_brackets_idx(line):
+    brackets_idx_list=[]
+    tmp_list = []
+    idx=0
+    for i in range(len(line)):
+        if line[idx]=='(':
+            tmp_list.append(idx)
+        elif line[idx]==')':
+            start_bracket_idx = tmp_list[-1]
+            tmp_list=tmp_list.pop(-1)
+            brackets_idx_list.append((start_bracket_idx, idx))
+    return brackets_idx_list
 
 
-def read_divide(letters, index):
-    token = {"type": "DEVIDE"}
-    return token, index + 1
 
 
-def parse_by_parentheses(letters):
-    letter_list = []
-    starting_parenthese_index_list = []
-    parentheses_index_list = []
-    for i in range(len(letters)):
-        letter_list.append(letters[i])
-        if letters[i] == "(":
-            starting_parenthese_index_list.append(i)
-        if letters[i] == ")":
-            starting_parenthese_index = starting_parenthese_index_list[-1]
-            parentheses_index_list.append((starting_parenthese_index, i))
-            starting_parenthese_index_list = starting_parenthese_index_list[:-1]
-    print("parentheses_index_list", parentheses_index_list)
-    return parentheses_index_list
+
