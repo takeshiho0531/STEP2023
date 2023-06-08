b235e1cbd1a66133fe3a407c49fa4a1c91e82d5a
takeshiho0531 <kawadatakeshiho@gmail.com>
2023-06-08 21:41:29 +0900

    feat: shortest_pathの表示できた

--- forth_week/wikipedia.py
+++ forth_week/wikipedia.py
@@ -1,11 +1,10 @@
 import sys
-import collections import deque
+from collections import deque
+
 
 class Wikipedia:
-
     # Initialize the graph of pages.
     def __init__(self, pages_file, links_file):
-
         # A mapping from a page ID (integer) to the page title.
         # For example, self.titles[1234] returns the title of the page whose
         # ID is 1234.
@@ -37,6 +36,10 @@ class Wikipedia:
         print("Finished reading %s" % links_file)
         print()
 
+    def get_id_from_title(self, title):
+        for key, val in self.titles.items():
+            if val == title:
+                return key
 
     # Find the longest titles. This is not related to a graph algorithm at all
     # though :)
@@ -51,7 +54,6 @@ class Wikipedia:
                 count += 1
             index += 1
         print()
-
 
     # Find the most linked pages.
     def find_most_linked_pages(self):
@@ -71,65 +73,63 @@ class Wikipedia:
         print()
 
     def BFS(self, start, goal):
-        """startのnodeとgoalのnodeを与えた時にstartとgoalを繋ぐ道があるのかを判定して、あった場合はgoalに辿り着くまでに最後に通ったnodeを返す
-        """
-        d=deque()
-        visited={}
-
+        d = deque()
+        start = self.get_id_from_title(start)
+        goal = self.get_id_from_title(goal)
         d.append(start)
-        visited[start]=True
+        visited = {}
+        visited[start] = True
 
-
-        while not d.empty():
-            node_pair=d.pop()
-            node=node_pair[1]
-            last_node=node_pair[0]
-            if node==goal:
-                return last_node
+        while len(d) != 0:
+            node = d.pop()
+            if node == goal:
+                return visited
             for child in self.links[node]:
                 if not child in visited:
-                    visited[child]=True
-                    d.append((node, child))
-
-        return None
-
+                    d.append(child)
+                    visited[child] = node
+        return "not found"
 
     # Find the shortest path.
     # |start|: The title of the start page.
     # |goal|: The title of the goal page.
     def find_shortest_path(self, start, goal):
-        path=[]
-        path.append(goal)
+        assert self.BFS(start, goal) != "not found"
+        visited_dict = self.BFS(start, goal)
+        start_id = self.get_id_from_title(start)
+        goal_id = self.get_id_from_title(goal)
+        node_id = goal_id
+        path = []
+        path.append(goal_id)
 
-        last_node=goal
-        while last_node!=start:
-            if self.BFS(start, goal=last_node) is None:
-                break
-            else:
-                last_node=self.BFS(start, goal=last_node)
-                path.append(last_node)
-        else:
-            if last_node==start:
-                path.append(start)
-                return path.reverse()
-            else:
-                exit(1)
+        while node_id != start_id:
+            last_node_id = visited_dict[node_id]
 
-
+            if last_node_id == start_id:
+                path.append(last_node_id)
+                path_title = []
+                for i in range(len(path)):
+                    node_title = self.titles[path[i]]
+                    path_title.append(node_title)
+                path_title.reverse()
+                print(path_title)
+                return
+            if last_node_id != start_id:
+                path.append(last_node_id)
+                node_id = last_node_id
 
     # Calculate the page ranks and print the most popular pages.
     def find_most_popular_pages(self):
-        #------------------------#
+        # ------------------------#
         # Write your code here!  #
-        #------------------------#
+        # ------------------------#
         pass
-
 
     # Do something more interesting!!
     def find_something_more_interesting(self):
-        #------------------------#
+        # ------------------------#
         # Write your code here!  #
-        #------------------------#
+        # ------------------------#
         pass
 
 
@@ -140,6 +140,8 @@ if __name__ == "__main__":
 
     wikipedia = Wikipedia(sys.argv[1], sys.argv[2])
     wikipedia.find_longest_titles()
-    wikipedia.find_most_linked_pages()
+    # wikipedia.find_most_linked_pages()
     wikipedia.find_shortest_path("渋谷", "パレートの法則")
-    wikipedia.find_most_popular_pages()
+    # wikipedia.BFS("A", "C")
+    # wikipedia.find_shortest_path("A", "C")
+    # wikipedia.find_most_popular_pages()
